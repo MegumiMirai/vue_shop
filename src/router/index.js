6ea8d72fca79ce42f0fb2426abc7f33fb2a0c409
勿忘草与永远的西莲寺紫 <2261768355@qq.com>
2022-06-16 21:21:00 +0800

    提交代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import axios from 'axios'
+
 // 导入组件
 import Login from '@/components/Login.vue'
 import Home from '@/components/Home.vue'
+import Welcome from '@/components/Welcome.vue'
 
 Vue.use(VueRouter)
-
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-Vue.prototype.$http = axios
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome }
+    ]
+  }
 ]
 
 const router = new VueRouter({
